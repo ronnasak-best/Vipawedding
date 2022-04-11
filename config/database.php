89c199a18700d05fb35746a422c99a45a139fce0
ronnasak-best <Ronnasak_best@hotmail.com>
2022-04-12 01:37:42 +0700

    2

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => 'https://fast-savannah-75931.herokuapp.com',
-            'host' => 'us-cdbr-east-05.cleardb.net',
-            'port' => '3306',
-            'database' => 'heroku_d27e2ab332cb0d2',
-            'username' => 'b36b815bec3ba1',
-            'password' => 'cef9c0b7',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
